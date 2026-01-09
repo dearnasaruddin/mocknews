41996d9f145cbfab1a14ecedfad8410a179c46e5
dearnasaruddin <dearnasaruddin@gmail.com>
2026-01-09 17:34:28 +0600

    desktop nav items fixed to the center

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
     };
   }, []);
 
-  
+
   return (
     pathname.includes('/auth') ?
       <></>
@@ -47,13 +47,39 @@ const Navbar = () => {
           <div>
             <Link href={"/"} className='font-bold text-xl lg:text-2xl'>mock News</Link>
           </div>
+          {/* ============= Desktop Navigation Menu Items ============= */}
+          <NavigationMenu className='hidden lg:flex' viewport={false}>
+            <NavigationMenuList>
+              <NavigationMenuItem className='lg:flex items-center space-x-8 max-md:space-y-2'>
+                <NavigationMenuLink className={`${pathname == '/news' ? 'text-red-500' : 'hover:text-red-500'}`} asChild>
+                  <Link href='/news'>News</Link>
+                </NavigationMenuLink>
 
+                <NavigationMenuTrigger className={`${pathname == '/services' ? 'text-red-500' : 'hover:text-red-500'} bg-transparent text-sm`} >
+                  Services
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className='text-gray-600  rounded-md space-y-2'>
+                    <li><NavigationMenuLink href='/services/web' className='px-4'>Web Development</NavigationMenuLink></li>
+                    <li><NavigationMenuLink href='/services/apps' className='px-4'>Mobile App</NavigationMenuLink></li>
+                    <li><NavigationMenuLink href='/services/seo' className='px-4'>SEO</NavigationMenuLink></li>
+                  </ul>
+                </NavigationMenuContent>
+
+                <NavigationMenuLink className={`${pathname == '/about' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link href='/about'>About</Link></NavigationMenuLink>
+                <NavigationMenuLink className={`${pathname == '/contact' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link href='/contact'>contact</Link></NavigationMenuLink>
+
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
 
           {/* =========== Navigation Menu =========== */}
           <div ref={navMenuRef} className={` ${isMenuOpen ? ' max-md:right-0' : 'hidden max-md:-right-full'} lg:flex lg:items-center lg:justify-between lg:gap-6 duration-200  max-md:absolute max-md:top-0 max-md:w-[60vw] max-md:bg-gray-100 max-md:dark:bg-gray-800 max-md:shadow-2xl max-md:h-screen max-md:p-8 max-md:z-40`}>
             <IoMdCloseCircleOutline onClick={() => setIsMenuOpen(false)} className='absolute top-5 right-5 text-2xl lg:hidden' />
-            <div className='max-md:mt-4'>
-              <NavigationMenu className='lg:flex' viewport={false}>
+
+            {/* ============ Mobile Navigation Menu Items =========== */}
+            <div className='lg:hidden max-md:mt-4'>
+              <NavigationMenu viewport={false}>
                 <NavigationMenuList>
                   <NavigationMenuItem className='lg:flex items-center space-x-8 max-md:space-y-2'>
                     <NavigationMenuLink className={`${pathname == '/news' ? 'text-red-500' : 'hover:text-red-500'}`} asChild>
