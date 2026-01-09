186f5ca0799068cabfe210313625915350cacc08
dearnasaruddin <dearnasaruddin@gmail.com>
2026-01-09 17:20:03 +0600

    mobile responsive nav drawer design & outside click issue fixed

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -20,10 +20,23 @@ import { IoMdCloseCircleOutline } from 'react-icons/io'
 const Navbar = () => {
   const pathname = usePathname()
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  }
+  const navMenuRef = useRef<HTMLDivElement>(null);
 
+  // ========== Close menu on outside click ===========
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (navMenuRef.current && !navMenuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  
   return (
     pathname.includes('/auth') ?
       <></>
@@ -35,15 +48,16 @@ const Navbar = () => {
             <Link href={"/"} className='font-bold text-xl lg:text-2xl'>mock News</Link>
           </div>
 
+
           {/* =========== Navigation Menu =========== */}
-          <div className={` ${isMenuOpen ? ' max-md:right-0' : 'hidden max-md:-right-full'} lg:flex lg:items-center lg:justify-between lg:gap-6 duration-200  max-md:absolute max-md:top-0 max-md:flex max-md:flex-col max-md:justify-between max-md:w-[60vw] max-md:bg-gray-100 max-md:dark:bg-gray-800 max-md:shadow-2xl max-md:h-screen max-md:p-8 max-md:z-50`}>
-            <IoMdCloseCircleOutline onClick={toggleMenu} className='absolute top-5 right-5 text-2xl lg:hidden' />
-            <div >
+          <div ref={navMenuRef} className={` ${isMenuOpen ? ' max-md:right-0' : 'hidden max-md:-right-full'} lg:flex lg:items-center lg:justify-between lg:gap-6 duration-200  max-md:absolute max-md:top-0 max-md:w-[60vw] max-md:bg-gray-100 max-md:dark:bg-gray-800 max-md:shadow-2xl max-md:h-screen max-md:p-8 max-md:z-40`}>
+            <IoMdCloseCircleOutline onClick={() => setIsMenuOpen(false)} className='absolute top-5 right-5 text-2xl lg:hidden' />
+            <div className='max-md:mt-4'>
               <NavigationMenu className='lg:flex' viewport={false}>
                 <NavigationMenuList>
                   <NavigationMenuItem className='lg:flex items-center space-x-8 max-md:space-y-2'>
                     <NavigationMenuLink className={`${pathname == '/news' ? 'text-red-500' : 'hover:text-red-500'}`} asChild>
-                      <Link href='/news'>News</Link>
+                      <Link onClick={() => setIsMenuOpen(false)} href='/news'>News</Link>
                     </NavigationMenuLink>
 
                     <NavigationMenuTrigger className={`${pathname == '/services' ? 'text-red-500' : 'hover:text-red-500'} bg-transparent text-sm`} >
@@ -57,8 +71,8 @@ const Navbar = () => {
                       </ul>
                     </NavigationMenuContent>
 
-                    <NavigationMenuLink className={`${pathname == '/about' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link href='/about'>About</Link></NavigationMenuLink>
-                    <NavigationMenuLink className={`${pathname == '/contact' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link href='/contact'>contact</Link></NavigationMenuLink>
+                    <NavigationMenuLink className={`${pathname == '/about' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link onClick={() => setIsMenuOpen(false)} href='/about'>About</Link></NavigationMenuLink>
+                    <NavigationMenuLink className={`${pathname == '/contact' ? 'text-red-500' : 'hover:text-red-500'}`} asChild><Link onClick={() => setIsMenuOpen(false)} href='/contact'>contact</Link></NavigationMenuLink>
 
                   </NavigationMenuItem>
                 </NavigationMenuList>
@@ -66,15 +80,15 @@ const Navbar = () => {
             </div>
 
             {/* =========== Theme Switcher & Login Btn =========== */}
-            <div className='flex items-center max-md:justify-center'>
+            <div className='flex items-center max-md:justify-center max-md:mt-6 max-md:pt-6 max-md:border-t max-md:border-gray-300 max-md:dark:border-gray-700 '>
               <ModeToggle />
-              <Link href="/auth/login" className='ml-4 py-2 px-6 rounded-md text-sm font-medium bg-[#0f172b] dark:bg-[#e2e8f0] hover:bg-gray-700 dark:hover:bg-gray-300 duration-200 text-[#f9fafb] dark:text-[#3e4657]'>Login</Link>
+              <Link onClick={() => setIsMenuOpen(false)} href="/auth/login" className='ml-4 py-2 px-6 rounded-md text-sm font-medium bg-[#0f172b] dark:bg-[#e2e8f0] hover:bg-gray-700 dark:hover:bg-gray-300 duration-200 text-[#f9fafb] dark:text-[#3e4657]'>Login</Link>
             </div>
           </div>
 
           {/* Mobile menu button */}
           <div className='lg:hidden'>
-            <Button onClick={toggleMenu} variant="ghost">
+            <Button onClick={() => setIsMenuOpen(true)} variant="ghost">
               <CgMenuRightAlt className=' size-6' />
             </Button>
           </div>
